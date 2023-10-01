63db7ea78f4c32b8338a9b84cdd46b0d3bf27ff3
Meep <supermeip@gmail.com>
2023-10-01 18:46:48 -0400

    Adding base rules

--- libs/langs/astra/parsers/astra-parser/target/debug/deps/astra_parser-a625347a9f119ab5.d
+++
@@ -1,5 +0,0 @@
-/root/Meep-Tech/apps/Indra/libs/langs/astra/parsers/astra-parser/target/debug/deps/astra_parser-a625347a9f119ab5.rmeta: src/main.rs
-
-/root/Meep-Tech/apps/Indra/libs/langs/astra/parsers/astra-parser/target/debug/deps/astra_parser-a625347a9f119ab5.d: src/main.rs
-
-src/main.rs:
--- libs/langs/astra/parsers/astra-parser/target/debug/deps/astra_parser-cfa750e7598e7767.d
+++
@@ -1,5 +0,0 @@
-/root/Meep-Tech/apps/Indra/libs/langs/astra/parsers/astra-parser/target/debug/deps/astra_parser-cfa750e7598e7767.rmeta: src/main.rs
-
-/root/Meep-Tech/apps/Indra/libs/langs/astra/parsers/astra-parser/target/debug/deps/astra_parser-cfa750e7598e7767.d: src/main.rs
-
-src/main.rs:
